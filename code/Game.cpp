26ddc15dd2c2857529f62227cee3f841291036de
Bruno Cabral <cabral.brc@gmail.com>
2024-07-31 08:52:20 -0700

    InitAudioDevice is called from Rygame
    Signed-off-by: Bruno Cabral <cabral.brc@gmail.com>

--- code/Game.cpp
+++ code/Game.cpp
@@ -6,7 +6,6 @@ Game::Game(const int width, const int height)
     rg::Init();
     rg::display::SetMode(width, height);
     rg::display::SetCaption("Sprout Land");
-    rl::InitAudioDevice();
 
     // need to init level after rg::display::SetMode as it calls rl::InitWindow()
     // and InitWindow starts raylib resources that are needed in Level()
